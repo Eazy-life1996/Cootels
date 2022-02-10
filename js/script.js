a930cbf4d8e7819f709530e5238e6497b75a1713
Yurii <eazylifemusic@gmail.com>
2022-02-10 17:25:48 +0600

    разработал футер

--- js/script.js
+++ js/script.js
@@ -4,3 +4,21 @@ $(document).ready(function () {
     arrows: false
   });
 });
+// let btn = document.querySelector('.js-btn');
+// let btnSecond = document.querySelector('.js-btn-2');
+// let btnThird = document.querySelector('.js-btn-3');
+// let list = document.querySelector('.footer__list');
+// let listSecond = document.querySelector('.footer__list-2');
+// let listThird = document.querySelector('.footer__list-3');
+
+// btn.addEventListener("click", function () {
+//   list.classList.toggle('footer__list');
+// });
+
+// btnSecond.addEventListener("click", function () {
+//   listSecond.classList.toggle('footer__list-2');
+// });
+
+// btnThird.addEventListener("click", function () {
+//   listThird.classList.toggle('footer__list-3');
+// });
