9f6190f87bb081e425aa5a0fc151a9ad680eb0f3
Yurii <eazylifemusic@gmail.com>
2022-02-11 13:59:33 +0600

    Доделал футер реализовал раскрытие списков на моб версии и не большие правки

--- js/script.js
+++ js/script.js
@@ -4,21 +4,30 @@ $(document).ready(function () {
     arrows: false
   });
 });
-// let btn = document.querySelector('.js-btn');
-// let btnSecond = document.querySelector('.js-btn-2');
-// let btnThird = document.querySelector('.js-btn-3');
-// let list = document.querySelector('.footer__list');
-// let listSecond = document.querySelector('.footer__list-2');
-// let listThird = document.querySelector('.footer__list-3');
 
-// btn.addEventListener("click", function () {
-//   list.classList.toggle('footer__list');
-// });
+//Выше настройка слайдера!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-// btnSecond.addEventListener("click", function () {
-//   listSecond.classList.toggle('footer__list-2');
-// });
+let btn = document.querySelector('.js-btn');
+let btnSc = document.querySelector('.js-btn-2');
+let btnTh = document.querySelector('.js-btn-3');
+let list = document.querySelector('.footer__list');
+let listSc = document.querySelector('.footer__list-2');
+let listTh = document.querySelector('.footer__list-3');
 
-// btnThird.addEventListener("click", function () {
-//   listThird.classList.toggle('footer__list-3');
-// });
+function firstClick () {
+  list.classList.toggle('footer__list');
+}
+
+function secondClick () {
+  listSc.classList.toggle('footer__list');
+}
+
+function thirdClick () {
+  listTh.classList.toggle('footer__list');
+}
+
+btn.addEventListener("click", firstClick);
+btnSc.addEventListener("click", secondClick);
+btnTh.addEventListener("click", thirdClick);
+
+//Выше реализовано раскрытие списков в футере!!!!!!!!!!!!!
