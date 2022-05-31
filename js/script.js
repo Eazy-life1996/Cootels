5421f365b74547f4262cb6b5193bae330fe5367c
Yurii <eazylifemusic@gmail.com>
2022-05-31 14:25:31 +0600

    Реализовал правильное открытие списков в футере

--- js/script.js
+++ js/script.js
@@ -7,27 +7,12 @@ $(document).ready(function () {
 
 //Выше настройка слайдера!!!!!!!!!!!!!!!!!!!!!!!!!!!!
 
-let btn = document.querySelector('.js-btn');
-let btnSc = document.querySelector('.js-btn-2');
-let btnTh = document.querySelector('.js-btn-3');
-let list = document.querySelector('.footer__list');
-let listSc = document.querySelector('.footer__list-2');
-let listTh = document.querySelector('.footer__list-3');
+let btn = document.querySelectorAll('.footer__button');
 
-function firstClick () {
-  list.classList.toggle('footer__list');
-}
-
-function secondClick () {
-  listSc.classList.toggle('footer__list');
-}
-
-function thirdClick () {
-  listTh.classList.toggle('footer__list');
-}
-
-btn.addEventListener("click", firstClick);
-btnSc.addEventListener("click", secondClick);
-btnTh.addEventListener("click", thirdClick);
+btn.forEach(btn => {
+  btn.addEventListener('click', function () {
+    btn.nextElementSibling.classList.toggle('footer__list');
+  });
+});
 
 //Выше реализовано раскрытие списков в футере!!!!!!!!!!!!!
